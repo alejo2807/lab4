67f8234b5c70a0c6690223a9369e33367db320f5
David <davidgarciag2807@gmail.com>
2025-07-12 15:27:25 -0500

    Optimizacion de la vista del juego

--- VistaConsola.cpp
+++ VistaConsola.cpp
@@ -12,93 +12,91 @@
 VistaConsola::VistaConsola(ITablero* tablero, IPersonaje* avatar):tablero(tablero), avatar(avatar){}
 
 void VistaConsola::mostrarTablero() {
-    limpiarPantalla();
-    std::cout<<"Fil--Col\t";
-    for(int i=0; i<tablero->getSize();i++) std::cout<<i<<"\t";
-    std::cout<<"\n";
+	limpiarPantalla();
+	std::cout<<"Fil--Col\t";
+	for(int i=0; i<tablero->getSize();i++) std::cout<<i<<"\t";
+	std::cout<<"\n";
 
-    for(int i=0; i<tablero->getSize();i++){
-        std::cout<<i<<"\t\t";
-        for(int j=0; j<tablero->getSize();j++){ 
-            Ficha* ficha = tablero->getFicha(i,j);
-            std::cout<<ficha->getTipo()<<"\t";
-        }
-        std::cout<<"\n";
-    }
+	for(int i=0; i<tablero->getSize();i++){
+		std::cout<<i<<"\t\t";
+		for(int j=0; j<tablero->getSize();j++){ 
+			Ficha* ficha = tablero->getFicha(i,j);
+			std::cout<<ficha->getTipo()<<"\t";
+		}
+		std::cout<<"\n";
+	}
 
 }
 void VistaConsola::limpiarPantalla(){
-    #ifdef _WIN32
-        system("cls"); // Comando para Windows
-    #else
-        system("clear"); // Comando para Linux y macOS
-    #endif
+	#ifdef _WIN32
+		system("cls"); // Comando para Windows
+	#else
+		system("clear"); // Comando para Linux y macOS
+	#endif
 }
 
-void VistaConsola::mostrarJuego(){
+void VistaConsola::mostrarJuego() {
     limpiarPantalla();
-
-
-    for(int i=0; i<tablero->getSize();i++){
-        for(int j=0; j<tablero->getSize();j++){ 
-            if(avatar->getPosicionFila()==i and avatar->getPosicionColumna()==j) {
-                   std::cout << "🟫🟫🟩🧝🟫🟫" <<"\t";
-            }
-            else {
-                Ficha* ficha = tablero->getFicha(i,j);
-                if(ficha->getTipo()=='1') {
-                 std::srand(std::time(0));
-                 // Generar un número aleatorio (0 o 1)
-                int randomValue = std::rand() % 2;
-                if(randomValue==0) std::cout << "🟫🟫🟩🟨🟫🟫" <<"\t";
-                else std::cout << "🟫🟫🟨🟩🟫🟫" <<"\t";
-
-            }
-                else if(ficha->getTipo()=='0') std::cout << "🟫🟫🟦🟦🟫🟫" <<"\t";
-                else std::cout << "🟫🟫💰💰🟫🟫" <<"\t";
-                
+    
+    for(int i = 0; i < tablero->getSize(); i++) {
+        for(int j = 0; j < tablero->getSize(); j++) {
+            if(i == avatar->getPosicionFila() && j == avatar->getPosicionColumna()) {
+                std::cout << "🧝 ";
+                continue;
             }
             
+            Ficha* ficha = tablero->getFicha(i,j);
+            if(!ficha) {
+                std::cout << "  ";
+                continue;
+            }
+            
+            switch(ficha->getTipo()) {
+                case '1': std::cout << (rand() % 2 ? "🟩 " : "🟨 "); break;
+                case '0': std::cout << "🟦 "; break;
+                case 'S': std::cout << "💰 "; break;
+                default:  std::cout << "  ";
+            }
         }
-        std::cout<<"\n";
+        std::cout << "\n";
     }
-
 }
 
 
 // Implementación de mostrarMensaje
 void VistaConsola::mostrarMensaje(const std::string& mensaje) {
 
-    std::cout << mensaje << "\n";
+	std::cout << mensaje << "\n";
 }
 
 // Implementación de leerEntrada
 char VistaConsola::getEntradaConsola() {
-    char entrada;
-    bool entradaValida = false;
-    struct termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
+	char entrada;
+	bool entradaValida = false;
+	struct termios oldt, newt;
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
 
-    // Deshabilitar el modo de espera para Enter
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	// Deshabilitar el modo de espera para Enter
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-    do {
-    std::cout << "Ingrese una opción [aA-Izquierda] [wW-Arriba] [sS-Abajo] [dD-Derecha]: ";
-    entrada=getchar();
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	do {
+	std::cout << "Ingrese una opción [aA-Izquierda] [wW-Arriba] [sS-Abajo] [dD-Derecha]\n [qQ-Diagonal Arriba Izq] [eE-Diagonal Arriba Der] [zZ-Diagonal Abajo Izq] [cC-Diagonal Abajo Der]: ";
+	entrada=getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 
-        // Convertir a minúscula para simplificar validación
-        char entradaMin = std::tolower(entrada);
+		// Convertir a minúscula para simplificar validación
+		char entradaMin = std::tolower(entrada);
 
-        // Validar que la entrada sea 'w', 'a', 's' o 'd'
-        if (entradaMin == 'w' || entradaMin == 'a' || entradaMin == 's' || entradaMin == 'd') {
-            entradaValida = true;
-        } else {
-            std::cout << "Entrada inválida. Por favor, introduce wW, aA, sS o dD.\n";
-        }
-    } while (!entradaValida);
+		// Validar que la entrada sea 'w', 'a', 's' o 'd' con los movimientos diagonales
+		if (entradaMin == 'w' || entradaMin == 'a' || entradaMin == 's' || entradaMin == 'd' 
+			|| entradaMin == 'q' || entradaMin == 'e' || entradaMin == 'c' || entradaMin == 'z') {
+			entradaValida = true;
+		} else {
+			std::cout << "Entrada inválida. Por favor, introduce wW, aA, sS, dD, qQ, eE, zZ o cC.\n";
+		}
+	} while (!entradaValida);
 
-    return entrada;
+	return entrada;
 }
