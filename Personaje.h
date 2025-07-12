e555182328005c499d34a059f3365ea3ef9da655
David <davidgarciag2807@gmail.com>
2025-07-12 15:26:04 -0500

    Implementacion del estado del personaje (vivo o muerto)
    Esto hay que trabajarlo mas porque no funciona como deberia ser.
    Aun existe el problema de que el personaje "cae en un precipicio"
    y no fallece

--- Personaje.h
+++ Personaje.h
@@ -6,21 +6,26 @@
 
 class Personaje : public IPersonaje {
 protected:
-    int posicionFila;
-    int posicionColumna;
+	int posicionFila;
+	int posicionColumna;
+	bool estaVivo;
 
 public:
-    Personaje();
-    virtual ~Personaje() = default;
+	Personaje();
+	virtual ~Personaje() = default;
 
-    // Métodos de la interfaz IPersonaje
-    virtual int getPosicionFila() const override;
-    virtual int getPosicionColumna() const override;
-    virtual void mover(char direccion) override;
+	// Métodos de la interfaz IPersonaje
+	virtual int getPosicionFila() const override;
+	virtual int getPosicionColumna() const override;
+	virtual void mover(char direccion) override;
+	// Método para verificar si el personaje está vivo
+	virtual bool getEstaVivo() const { return estaVivo; }
 
-    // Implementación de los métodos setPosicionFila y setPosicionColumna
-    virtual void setPosicionFila(int fila) override;
-    virtual void setPosicionColumna(int columna) override;
+	// Implementación de los métodos setPosicionFila y setPosicionColumna
+	virtual void setPosicionFila(int fila) override;
+	virtual void setPosicionColumna(int columna) override;
+	virtual void setEstado(bool estado) { estaVivo = estado; }
+
 };
 
 #endif // PERSONAJE_H
