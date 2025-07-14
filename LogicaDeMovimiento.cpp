bbe242e25ec1d8fe2c5a12276fc3fd7a4e7e4b80
David <davidgarciag2807@gmail.com>
2025-07-14 15:35:02 -0500

    Version mejorada de LogicaDeMovimiento
    Ahora esta implementado AvatarCPU y AvatarInnovador

--- LogicaDeMovimiento.cpp
+++ LogicaDeMovimiento.cpp
@@ -12,25 +12,8 @@ void LogicaDeMovimiento::mover(IPersonaje &personaje, char direccion, const ITab
     int nuevaFila = personaje.getPosicionFila();
     int nuevaColumna = personaje.getPosicionColumna();
 
-<<<<<<< HEAD
-	/*
-	
-	if (!esAvatarCPU)
-	{
-	si no es avatar cpu, meter el switch completo aqui, ya que es avatar normal o avatar innovador
-	el personaje se puede mover en las 4 direcciones con sus diagonlaes (si es innovador, sino, solo en 4 direcciones)
-	}
-	
-	else
-	{
-	si es avatar cpu, meter la funcion que permite mover de forma random. 
-	Le podemos pedir al usuario que ingrese una tecla para que el cpu se mueva solo, por ejemplo.
-	Pero eso no implica que el usuario mueva el avatar cpu
-	)*/
-=======
     bool esAvatarInnovador = (dynamic_cast<AvatarInnovador*>(&personaje) != nullptr);
     bool esAvatarCPU = (dynamic_cast<AvatarCPU*>(&personaje) != nullptr);
->>>>>>> 65203f813fd2afd6e609e79a3f5591ed94efd014
 
     // Si es AvatarInnovador
     if (esAvatarInnovador) {
@@ -54,38 +37,6 @@ void LogicaDeMovimiento::mover(IPersonaje &personaje, char direccion, const ITab
         int filaActual = nuevaFila;
         int columnaActual = nuevaColumna;
 
-<<<<<<< HEAD
-		//Agregamos los movimientos diagonales de avatar innovador
-		case 'Q': case 'q': if (esAvatarInnovador) { nuevaFila--; nuevaColumna--; } break;
-		case 'E': case 'e': if (esAvatarInnovador) { nuevaFila--; nuevaColumna++; } break;
-		case 'Z': case 'z': if (esAvatarInnovador) { nuevaFila++; nuevaColumna--; } break;
-		case 'C': case 'c': if (esAvatarInnovador) { nuevaFila++; nuevaColumna++; } break;
-		
-		//else{}
-		
-		default:
-			// Dirección no válida
-			return;
-	}
-	
-	// CORRECTED BOUNDS CHECKING
-	int dimension = tablero.getSize(); // Since board is square
-	if (nuevaFila < 0 || nuevaFila >= dimension || 
-		nuevaColumna < 0 || nuevaColumna >= dimension) {
-		return;
-	}
-
-	Ficha* target = tablero.getFicha(nuevaFila, nuevaColumna);
-	
-	// SAFETY CHECKS
-	if (!target) return; // Invalid position
-	
-	// Only update position if not abyss
-	if (target->getTipo() != '0') {
-		personaje.setPosicionFila(nuevaFila);
-		personaje.setPosicionColumna(nuevaColumna);
-	}
-=======
         while (!movimientoValido && intentos < 10) {
             int direccionAleatoria = rand() % 4;
             nuevaFila = filaActual;
@@ -126,5 +77,4 @@ void LogicaDeMovimiento::mover(IPersonaje &personaje, char direccion, const ITab
 
     personaje.setPosicionFila(nuevaFila);
     personaje.setPosicionColumna(nuevaColumna);
->>>>>>> 65203f813fd2afd6e609e79a3f5591ed94efd014
 }
