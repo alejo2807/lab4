f7483e58445bbc6d5040f4cf38c0ee63e636bbaa
David <davidgarciag2807@gmail.com>
2025-07-14 16:55:19 -0500

    Implementacion setter setAvatar(...)

--- VistaConsola.h
+++ VistaConsola.h
@@ -10,19 +10,24 @@
 // Implementación concreta de IVistaJuego
 class VistaConsola : public IVistaJuego {
 private:
-    ITablero* tablero;          // Puntero al tablero
-    IPersonaje* avatar;         // Puntero al personaje
+	ITablero* tablero;          // Puntero al tablero
+	IPersonaje* avatar;         // Puntero al personaje
 public:
-    // Constructor y destructor
-    VistaConsola(ITablero* tablero, IPersonaje* avatar);
-    ~VistaConsola() override = default;
+	// Constructor y destructor
+	VistaConsola(ITablero* tablero, IPersonaje* avatar);
+	~VistaConsola() override = default;
+	
+	//set del avatar (se usa en la eleccion de avatar)
+	void setAvatar(IPersonaje* nuevoAvatar) {
+        avatar = nuevoAvatar;
+    }
 
-    // Métodos que implementan IVistaJuego
-    void mostrarTablero() override;
-    void mostrarJuego() override;
-    void limpiarPantalla() override;
-    void mostrarMensaje(const std::string& mensaje) override;
-    char getEntradaConsola() override;
+	// Métodos que implementan IVistaJuego
+	void mostrarTablero() override;
+	void mostrarJuego() override;
+	void limpiarPantalla() override;
+	void mostrarMensaje(const std::string& mensaje) override;
+	char getEntradaConsola() override;
 };
 
 #endif // VISTA_CONSOLA_H
